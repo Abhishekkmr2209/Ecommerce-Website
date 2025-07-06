ac2a3ce788e1332a81b333a6de423765f234fbf5
Abhishek Kumar <abhishekkmr.2209@gmail.com>
2025-07-06 22:05:57 +0530

    products.js added

---
+++ products.js
@@ -0,0 +1,251 @@
+const products = [
+    {
+        id:1,
+        img:'img/products/f1.jpg',
+        brand:'nike',
+        productName:'Summer T Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:2,
+        img:'img/products/f2.jpg',
+        brand:'adidas',
+        productName:'Slinger Shirt',
+        price:'$48',
+        star:4
+    },
+    {
+        id:3,
+        img:'img/products/f3.jpg',
+        brand:'Louis Phillipe',
+        productName:'Summer T Shirts',
+        price:'$51',
+        star:2
+    },
+    {
+        id:4,
+        img:'img/products/n1.jpg',
+        brand:'Array',
+        productName:'Summer Lights',
+        price:'$18',
+        star:3
+    },
+    {
+        id:5,
+        img:'img/products/n3.jpg',
+        brand:'Armani',
+        productName:'Flipper Trends',
+        price:'$70',
+        star:4
+    },
+    {
+        id:6,
+        img:'img/products/n5.jpg',
+        brand:'puma',
+        productName:'Thongs Beam',
+        price:'$35',
+        star:2
+    },
+    {
+        id:7,
+        img:'img/products/f7.jpg',
+        brand:'amazon',
+        productName:'Amazon clothing',
+        price:'$10',
+        star:3
+    },
+    {
+        id:8,
+        img:'img/products/f8.jpg',
+        brand:'airZip',
+        productName:'Studded clothing',
+        price:'$36',
+        star:3
+    },
+    {
+        id:9,
+        img:'img/products/n1.jpg',
+        brand:'lacoste',
+        productName:'Summer T Shirts',
+        price:'$46',
+        star:3
+    },
+    {
+        id:10,
+        img:'img/products/n5.jpg',
+        brand:'louis phillipe',
+        productName:'Shirts Trimmed',
+        price:'$24',
+        star:3
+    },
+    {
+        id:11,
+        img:'img/products/f1.jpg',
+        brand:'nike',
+        productName:'Summer T Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:12,
+        img:'img/products/f2.jpg',
+        brand:'pawshop',
+        productName:'Pants',
+        price:'$45',
+        star:3
+    },
+    {
+        id:13,
+        img:'img/products/f5.jpg',
+        brand:'US Polo',
+        productName:'Beamers',
+        price:'$18',
+        star:1
+    },
+    {
+        id:14,
+        img:'img/products/f5.jpg',
+        brand:'UCB',
+        productName:'Shirts All-Weather',
+        price:'$36',
+        star:5
+    },
+    {
+        id:15,
+        img:'img/products/f8.jpg',
+        brand:'nike',
+        productName:'Summer T Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:16,
+        img:'img/products/n3.jpg',
+        brand:'Under Armour',
+        productName:'T Shirts',
+        price:'$50',
+        star:4
+    },
+    {
+        id:17,
+        img:'img/products/f4.jpg',
+        brand:'max',
+        productName:'Shirts HolidayWear',
+        price:'$59',
+        star:4
+    },
+    {
+        id:18,
+        img:'img/products/n1.jpg',
+        brand:'jubilee',
+        productName:'T Shirts',
+        price:'$108',
+        star:5
+    },
+    {
+        id:19,
+        img:'img/products/n4.jpg',
+        brand:'allWear',
+        productName:'Breeps All',
+        price:'$36',
+        star:3
+    },
+    {
+        id:20,
+        img:'img/products/f1.jpg',
+        brand:'nike',
+        productName:'Summer T Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:21,
+        img:'img/products/f5.jpg',
+        brand:'newWear',
+        productName:'Polo T Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:22,
+        img:'img/products/n4.jpg',
+        brand:'nike',
+        productName:'Summer T Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:23,
+        img:'img/products/f1.jpg',
+        brand:'blackSum',
+        productName:'Printed T Shirts',
+        price:'$110',
+        star:3
+    },
+    {
+        id:24,
+        img:'img/products/n8.jpg',
+        brand:'blackSwan',
+        productName:'Full Sleeves',
+        price:'$56',
+        star:3
+    },{
+        id:25,
+        img:'img/products/f1.jpg',
+        brand:'nike',
+        productName:'Summer T Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:26,
+        img:'img/products/n7.jpg',
+        brand:'under Armour',
+        productName:'Summer Tip top ',
+        price:'$36',
+        star:3
+    },
+    {
+        id:27,
+        img:'img/products/f7.jpg',
+        brand:'noBrand',
+        productName:'Shirts',
+        price:'$16',
+        star:4
+    },
+    {
+        id:28,
+        img:'img/products/f1.jpg',
+        brand:'myntra',
+        productName:'lightweight clothing',
+        price:'$12',
+        star:2
+    },
+    {
+        id:29,
+        img:'img/products/n2.jpg',
+        brand:'nike',
+        productName:'Summer T Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:30,
+        img:'img/products/n1.jpg',
+        brand:'nike',
+        productName:'Blustone Shirts',
+        price:'$36',
+        star:3
+    },
+    {
+        id:31,
+        img:'img/products/f1.jpg',
+        brand:'nike',
+        productName:'Summer T Shirts',
+        price:'$36',
+        star:3
+    }
+
+];
+export default products;
