0f5c00db1818ded4247f6317e96dc8d156662d35
Abhishek Kumar <abhishekkmr.2209@gmail.com>
2025-07-10 09:04:42 +0530

    blogs.js inserted

---
+++ blogs.js
@@ -0,0 +1,43 @@
+const blogs = [
+    {
+       img:'img/blog/b1.jpg',
+       title:`Sheer Sophistication: The Back-Detail Dress`,
+       description:` A deep-dive into the experimental use of lightweight mesh fabrics paired with minimalist design. This piece highlights how strategic back cutouts and fine straps can elevate everyday wear into artful fashion.`
+    },
+    {
+       img:'img/blog/b2.jpg',
+       title:`Yin & Yang: The Harmony of Core Colors`,
+       description:`Showcasing tonal contrast with muted earth tones and black, this story explores the brand’s exploration of balancing aesthetics through shade and silhouette.`
+    },
+    {
+       img:'img/blog/b3.jpg',
+       title:`Retail Reset: Reimagining the In‑Store Experience`,
+       description:`An inside look at the latest store design experiment. From minimalist fixtures to dynamic color-coded displays, this post digs into how space layout influences consumer behavior.
+
+` 
+    },
+    {
+       img:'img/blog/b4.jpg',
+       title:`Fall Flow: Draped Cardigan Innovations`,
+       description:`Introducing a soft draping technique and asymmetrical hems, this cardigan is a study in innovative layering—blending comfort with avant-garde simplicity.
+
+` 
+    },
+    {
+       img:'img/blog/b5.jpg',
+       title:`Seasonal Synergy: Knit Meets Nature`,
+       description:`A launch story on nature-inspired knitwear. Featuring mustard yarns and autumn textures, this piece illuminates how seasonal palette and tactile yarns shape new collections.` 
+    },
+    {
+       img:'img/blog/b6.jpg',
+       title:`Autumn Rituals: Warmth in Small Moments`,
+       description:`Discover our cozy knitwear, inspired by slow mornings, warm drinks, and the golden hues of fall. A tactile experience, designed for serenity.` 
+    },
+    {
+       img:'img/blog/b7.jpg',
+       title:`Back Details That Make a Statement`,
+       description:`Precision tailoring meets feminine detailing in this silk-back tie top. Engineered for elegance with technical fabric that holds its flowy drape.` 
+    }
+];
+
+export default blogs;
