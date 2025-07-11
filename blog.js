d2fdc1e6524dacf28b49b64e44ec13cd23759d99
Abhishek Kumar <abhishekkmr.2209@gmail.com>
2025-07-11 18:08:01 +0530

    blogs Added and pagination bug fixed

--- blog.js
+++ blog.js
@@ -32,7 +32,7 @@ function pagination(){
         button.textContent = i;
         button.addEventListener('click',()=>{
             currentPage=currPage;
-            displayBlog(currentPage);
+            displayBlog(currentPage,perPage);
         });
         }
         button.classList.add('paginationButton');
@@ -46,10 +46,81 @@ function pagination(){
 // Displaying blogs as per currentPage
 
 function displayBlog(currentPage,perPage){
+
+    const blogContainer = document.querySelector('#blogContainer');
+    blogContainer.innerHTML = "";
     const start = (currentPage-1)*perPage;
     const end  = start + perPage;
     
     const currentBlogsArray = blogs.slice(start,end);
+
+    currentBlogsArray.forEach((blog)=>{
+       //blogCard Container
+       const blogCard = document.createElement('div');
+       blogCard.classList.add('blogCard');
+       //Appending blogCard to blogContainer
+       blogContainer.appendChild(blogCard);
+
+       //div containing blogImage
+       const blogImageDiv = document.createElement('div');
+       blogImageDiv.classList.add('blogImageDiv');
+       //Appending to blogCard
+       blogCard.appendChild(blogImageDiv);
+
+       //creating img element for blogImageDiv
+        const blogImage = document.createElement('img');
+        blogImage.src = blog.img; 
+
+        //Appending blogImage to blogImageDiv
+        blogImageDiv.appendChild(blogImage);
+
+        //Creating desciptionDiv
+        const descriptionDiv = document.createElement('div');
+        descriptionDiv.classList.add('description');
+
+        //Appending descriptionDiv to blogCard
+        blogCard.appendChild(descriptionDiv);
+
+        //descriptionDiv Elements
+        //h4Element
+        const h4title = document.createElement('h4');
+        h4title.textContent = blog.title;
+
+        //Appending h4
+        descriptionDiv.appendChild(h4title);
+
+        //Describing the blog
+        const blogRead = document.createElement('p');
+        blogRead.textContent = blog.blogRead;
+
+        //Appending blogRead to descriptionDiv
+        descriptionDiv.appendChild(blogRead);
+
+        //Hidden blogRead part
+        const blogReadMore = document.createElement('span');
+        blogReadMore.classList.add('conReadingText');
+        blogReadMore.textContent = blog.blogReadMore;
+
+        //Appending hidden part to original
+        blogRead.appendChild(blogReadMore);
+
+        //Creating anchor 
+        const contRead = document.createElement('a');
+        contRead.href="#";
+        contRead.textContent = "CONTINUE READING..."
+        
+        //Appending it to descriptionDiv
+        descriptionDiv.appendChild(contRead);
+
+        //Creating Date
+        const dateElement = document.createElement('h1');
+        dateElement.textContent= blog.date;
+
+        //Appending Date
+        blogCard.appendChild(dateElement);
+
+    });
+
     
 }
 
@@ -57,4 +128,7 @@ function displayBlog(currentPage,perPage){
 
 window.addEventListener('load',()=>{
     pagination();
+    displayBlog(1, 5);
 });
+
+
