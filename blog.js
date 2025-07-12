ae451291067638e074124b27a8f59e2967c46c46
Abhishek Kumar <abhishekkmr.2209@gmail.com>
2025-07-12 19:20:48 +0530

    hamburger added to blogpage

--- blog.js
+++ blog.js
@@ -1,6 +1,22 @@
 import blogs from './blogDescription.js';
 
 
+/* Hamburger Menu */
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
 
 // Pagination
 
