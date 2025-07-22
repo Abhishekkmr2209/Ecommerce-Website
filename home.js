55ebab38f0289588199b67a261540f54fd933236
Abhishek Kumar <abhishekkmr.2209@gmail.com>
2025-07-22 23:14:24 +0530

    home.js added

---
+++ home.js
@@ -0,0 +1,34 @@
+/* Hamburger Menu */
+
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+//Upon Page refreshing cart items count
+    function cartItemsCountDisplay(){
+        if(!localStorage.getItem('numberOfItems')){
+            //Do nothing
+        }else{
+            const cartIcon = document.querySelectorAll('#header span');
+            cartIcon.forEach((spanElement)=>{
+            spanElement.classList.remove('cartCount');
+            spanElement.textContent=localStorage.getItem('numberOfItems');
+        });
+        }
+    }
+
+//Upon page loading
+window.addEventListener('load',()=>{
+    cartItemsCountDisplay();       //Display the total no of items in cart upon page load or refresh
+});
